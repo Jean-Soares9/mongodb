2b6693e3e0698edc6aa8b5bf112b8c468d24bbd1
JEAN ANDRADE SOARES <jean.asoares@senacsp.edu.br>
2025-02-20 20:07:53 -0300

    Criar Cliente

---
+++ main.js
@@ -0,0 +1,38 @@
+/**
+ * Processo principal
+ * Estudo do CRUD com MongoDB
+ */
+
+// importação do módulo de coneção (database.js)
+const {conectar, desconectar} = require('./database')
+
+// importação do modelo de dados de clientes
+const clienteModel = require('./src/models/clientes')
+
+// CRUD Create (função para adicionar um novo cliente)
+const criarCliente = async (nomeCli, foneCli) => {
+    try {
+        const novoCliente = new clienteModel(
+            {
+                nomeCliente: nomeCli,
+                foneCliente: foneCli
+            }
+        )
+            // a linha abaixo salva os dados do cliente no banco
+            await novoCliente.save()
+            console.log("Cliente adicionado com sucesso.")
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// execução da aplicação
+const app = async () => {
+    await conectar()
+    await criarCliente("Jean Andrade", "11912341234")
+    await desconectar()
+}
+
+console.clear()
+app()
