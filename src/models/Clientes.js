23f6dc4c86cf257cbbb0fb522d5abd07f723bd59
JEAN ANDRADE SOARES <jean.asoares@senacsp.edu.br>
2025-02-21 22:14:49 -0300

    Buscar Cliente

--- src/models/Clientes.js
+++ src/models/Clientes.js
@@ -14,8 +14,17 @@ const clienteSchema = new Schema({
     foneCliente: {
         type: String
     },
+    cpf: {
+        type: String,
+        unique: true,
+        index: true
+    },
+    dataCadastro: {
+        type: Date,
+        default: Date.now
+    }
 }, {versionKey: false})
 
 // importação do modelo de dados
-// obs: Clientes será o nome da coleção
+// obs: Clientes será o nome da coleção (mongdb -> clientes)
 module.exports = model('Clientes', clienteSchema)
